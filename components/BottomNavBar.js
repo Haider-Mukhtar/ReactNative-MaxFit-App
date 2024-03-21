90fca08e13a35207d0f15706bc2f2da15fb9c93d
Haider Mukhtar <haidermukhtarch405@gmail.com>
2024-03-21 00:53:42 -0700

    MaxFit v0.1.0

---
+++ components/BottomNavBar.js
@@ -0,0 +1,50 @@
+import { StyleSheet, Text, TextInput, TouchableOpacity, View, Image, KeyboardAvoidingView, ScrollView } from 'react-native'
+import React, { useState } from 'react'
+import { useNavigation } from '@react-navigation/native';
+
+const BottomNavBar = () => {
+
+    const navigation = useNavigation();
+
+    return (
+        <View style={{ marginTop: 20, borderTopColor: '#ADA4A5', borderTopWidth: 1, }}>
+            <View style={{ flexDirection: 'row', justifyContent: 'space-between', paddingHorizontal: 25, backgroundColor: '#fff', }}>
+                <TouchableOpacity
+                    style={{ paddingVertical: 25, }}>
+                    <Image
+                        source={require('../assets/icons/Home.png')}
+                    />
+                </TouchableOpacity>
+                <TouchableOpacity
+                    style={{ paddingVertical: 25, }}>
+                    <Image
+                        source={require('../assets/icons/Activity.png')}
+                    />
+                </TouchableOpacity>
+                <TouchableOpacity
+                    style={{ backgroundColor: '#92A3FD', borderRadius: 100, padding: 20, alignSelf: 'center', marginTop: -50 }}>
+                    <Image
+                        source={require('../assets/icons/Search.png')}
+                    />
+                </TouchableOpacity>
+                <TouchableOpacity
+                    style={{ paddingVertical: 25, }}>
+                    <Image
+                        source={require('../assets/icons/Camera.png')}
+                    />
+                </TouchableOpacity>
+                <TouchableOpacity
+                    style={{ paddingVertical: 25, }}>
+                    <Image
+                        source={require('../assets/icons/Profile.png')}
+                    />
+                </TouchableOpacity>
+            </View>
+        </View>
+
+    )
+}
+
+export default BottomNavBar
+
+const styles = StyleSheet.create({})
